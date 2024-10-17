949c00d3211ebb942276d1dcc6c83163408ecdcc
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-10-17 20:03:11 +0200

    add button component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -9,6 +9,7 @@ import HackerRoom from "../components/HackerRoom.jsx";
 import CanvasLoader from "../components/CanvasLoader.jsx";
 import Target from "../components/Target.jsx";
 import ReactLogo from "../components/ReactLogo.jsx";
+import Button from "../components/Button.jsx";
 import Rings from "../components/Rings.jsx";
 import Cube from "../components/Cube.jsx";
 import HeroCamera from "../components/HeroCamera.jsx";
@@ -53,6 +54,15 @@ const Hero = () => {
             <directionalLight position={[10, 10, 10]} intensity={0.5} />
           </Suspense>
         </Canvas>
+      </div>
+      <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
+        <a href="#about" className="w-fit">
+          <Button
+            name="Let's work together"
+            isBeam
+            containerClass="sm:w-fit w-full sm:min-w-96"
+          />
+        </a>
       </div>
     </section>
   );
