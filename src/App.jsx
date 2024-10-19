618f0c78411f6c592770b9df7d0a1e96e24c7d5a
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-10-19 16:52:53 +0200

    add about component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./sections/Navbar.jsx";
 import Hero from "./sections/Hero.jsx";
+import About from "./sections/About.jsx";
 
 const App = () => {
-    return (
-        <main className="max-w-7xl mx-auto">
-            <Navbar />
-            <Hero />
-        </main>
-    )
-}
-export default App
+  return (
+    <main className="max-w-7xl mx-auto">
+      <Navbar />
+      <Hero />
+      <About />
+    </main>
+  );
+};
+export default App;
